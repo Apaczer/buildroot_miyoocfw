a431579ce217f96eed23d6687d1e91bf8f579d93
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-09-25 17:35:26 +0200

    [MIMIC] bump version to 1.3.0.1
    thus latest/current mimic1 release, but build-in only "kal16" voice

--- package/mimic/mimic.mk
+++ package/mimic/mimic.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-MIMIC_VERSION = 1.1.0
-MIMIC_SITE = $(call github,MycroftAI,mimic,$(MIMIC_VERSION))
+MIMIC_VERSION = 1.3.0.1
+MIMIC_SITE = $(call github,MycroftAI,mimic1,$(MIMIC_VERSION))
 MIMIC_LICENSE = MIT
 MIMIC_LICENSE_FILES = COPYING
 
-MIMIC_DEPENDENCIES = host-pkgconf
++MIMIC_AUTORECONF = YES
++MIMIC_DEPENDENCIES = host-pkgconf host-automake host-autoconf host-libtool
 
 ifeq ($(BR2_PACKAGE_MIMIC_AUDIO_BACKEND_ALSA),y)
 MIMIC_AUDIO_BACKEND = alsa
@@ -24,6 +25,12 @@ else ifeq ($(BR2_PACKAGE_MIMIC_AUDIO_BACKEND_NONE),y)
 MIMIC_AUDIO_BACKEND = none
 endif
 
-MIMIC_CONF_OPTS += --with-audio=$(MIMIC_AUDIO_BACKEND)
+define MIMIC_RUN_AUTOGEN
+        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+
+MIMIC_PRE_CONFIGURE_HOOKS += MIMIC_RUN_AUTOGEN
+
+MIMIC_CONF_OPTS += --disable-voices-all --enable-cmu_us_kal --with-audio=$(MIMIC_AUDIO_BACKEND)
 
 $(eval $(autotools-package))
